33330c102e5ecf1f8a4e452b89f9d1bbe4cfb4ec
KWiefel <dev.kwiefel427@gmail.com>
2023-11-13 16:30:27 +0100

    Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,17 @@
 // Erstelle eine Methode, um die Karte in HTML anzuzeigen.
 // Style die Paninikarte mithilfe des Gridsystems.
 
+// Input
 const inputFirstName = document.body.querySelector("#firstName");
 const inputLastName = document.body.querySelector("#lastName");
 const inputNumberAlbum = document.body.querySelector("#numberAlbum");
 const inputAge = document.body.querySelector("#age");
 const inputUrl = document.body.querySelector("#inputUrl");
+// Output
+let output = document.body.querySelector("#output");
+const main = document.body.querySelector("main");
+
+let counter = 0;
 
 class Card {
   constructor(firstName, lastName, age, numberAlbum, imageUrl) {
@@ -21,7 +27,6 @@ class Card {
 }
 
 const addCard = () => {
-  let output = document.body.querySelector("#output");
   const newCard = new Card(
     inputFirstName.value,
     inputLastName.value,
@@ -29,10 +34,29 @@ const addCard = () => {
     inputNumberAlbum.value,
     inputUrl.value
   );
+
   output.innerHTML += `<div class="card__wrapper"><div class ="card__text">
   <h3>First Name: ${newCard.firstName}</h3>
   <h4>Last Name: ${newCard.lastName}</h4>
   <p>Number of Albums: ${newCard.numberAlbum} </p>
   <p>Age: ${newCard.age} </p></div>
   <img src=" ${newCard.imageUrl}" alt="ProfilePicture"></div>`;
+
+  if (counter === 0) {
+    addTitel();
+  }
+
+  counter++;
+  console.log(counter);
+};
+
+const addTitel = () => {
+  let cardTitle = document.createElement("h1");
+
+  if (cardTitle.innerText == "Added Cards:") {
+  } else {
+    cardTitle.innerText = "Added Cards:";
+    cardTitle.classList.add("title");
+    main.prepend(cardTitle);
+  }
 };
